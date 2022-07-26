d47ff5be14eb9d63c58f90d84d6b405d81d0688c
Federico Mahfoud <fedemahf@users.noreply.github.com>
2022-07-26 20:15:25 -0300

    Update entrypoint script

--- monero-wallet-cli.sh
+++ monero-wallet-cli.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-monero-wallet-cli --log-file /home/monero/wallet/monero-wallet-cli.log --untrusted-daemon --daemon-address node.supportxmr.com:18081
+monero-wallet-cli --log-file /home/monero/wallet/monero-wallet-cli.log --trusted-daemon --daemon-address localhost:18081
